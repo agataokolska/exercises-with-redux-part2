adf1d12a50bc629959535a1273f4f1a25c85a87a
agata okolska <agata.okolska@gmail.com>
2018-08-14 12:01:04 +0200

    add new redux duck - add.js

---
+++ src/state/add.js
@@ -0,0 +1,20 @@
+const ADD = 'add/ADD'
+
+export const addAction = number => ({ type: ADD, number })
+
+const initialState = {
+    result: 0
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD:
+            return {
+                ...state,
+                result: state.result + action.number
+            }
+        default:
+            return state
+
+    }
+}
--- src/store.js
+++ src/store.js
@@ -1,19 +1,21 @@
 import { createStore, combineReducers } from 'redux'
-import counterReducer, {incAction, decAction, addAction, substractAction} from './state/counter'
+import counterReducer, { incAction, decAction, addAction as addActionCounter, substractAction } from './state/counter'
+import add, {addAction} from './state/add'
 
 const reducer = combineReducers({
-    counter: counterReducer
-}) 
+    counter: counterReducer,
+    add
+})
 
 const store = createStore(
-    reducer, 
-   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-  
-  store.dispatch(addAction(6))
-  store.dispatch(addAction(10))
 
-  window.counterInc = () => store.dispatch(incAction())
-  window.counterDec = () => store.dispatch(decAction())
- 
+store.dispatch(addAction(3))
+store.dispatch(addActionCounter(10))
+
+
+window.counterInc = () => store.dispatch(incAction())
+window.counterDec = () => store.dispatch(decAction())
