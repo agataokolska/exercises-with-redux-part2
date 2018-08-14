d266cb0353c55c3b823271cecd6c2814b1bb1618
agata okolska <agata.okolska@gmail.com>
2018-08-14 14:14:33 +0200

    add action and reducer to calculator

--- src/state/calculator.js
+++ src/state/calculator.js
@@ -1,5 +1,25 @@
+const INPUT = 'calculator/INPUT'
+const ADD = 'calculator/ADD'
+const SUBSTRACT = 'calculator/SUBSTRACT'
+
+
+const inputAction = number => ({type:INPUT, number})
+
 const initialState = {
     result: 0,
     input: 0,
-    isResultShowed: false
+    isResultShown: false
+}
+
+export default (state=initialState, action)=>{
+    switch(action.type){
+        case INPUT:
+        return {
+            ...state,
+            input: action.number,
+            isResultShown: false
+        }
+        default:
+        return state
+    }
 }
