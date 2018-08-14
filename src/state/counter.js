95fdf1d2b842e2fd8c94555f3cb4d5cbe3142f13
agata okolska <agata.okolska@gmail.com>
2018-08-14 10:47:43 +0200

    add reducer in counter as export default

--- src/state/counter.js
+++ src/state/counter.js
@@ -1,7 +1,7 @@
 const INC = 'counter/INC'
 const DEC = 'counter/DEC'
 
-const incAction = () =>({ type: INC })
+const incAction = () => ({ type: INC })
 
 const decAction = () => ({ type: DEC })
 
@@ -9,3 +9,20 @@ const initialState = {
     number: 0
 }
 
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case INC:
+            return {
+                ...state,
+                number: state.number + 1
+            }
+        case DEC:
+            return {
+                ...state,
+                number: state.number - 1
+            }
+        default:
+            return state
+    }
+}
