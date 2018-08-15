f0f63109655d325f38206afa07f38729545c26a1
agata okolska <agata.okolska@gmail.com>
2018-08-15 09:13:44 +0200

    add buttons to Calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Counter from './views/Counter'
-
+import Calculator from './views/Calculator'
 class App extends React.Component {
   render() {
     return (
       <div>
         <Counter />
+        <Calculator />
       </div>
     )
   }
--- src/views/Calculator.js
+++ src/views/Calculator.js
@@ -3,6 +3,27 @@ import { connect } from 'react-redux'
 
 const Calculator = (props) => (
     <div>
+        <div>
+            <button>+</button>
+        </div>
+        <div>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+        </div>
+        <div>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+        </div>
+        <div>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+        </div>
+        <div>
+            <button>0</button>
+        </div>
 
     </div>
 )
